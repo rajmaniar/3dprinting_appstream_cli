19f0f3384030d4c2d48cb55a9e1d952eff740702
PewPewRaj <101134534+PewPewRaj@users.noreply.github.com>
2024-03-26 22:38:00 -0700

    Initial commit

--- main.go
+++ main.go
@@ -4,45 +4,84 @@ import (
 	"context"
 	"flag"
 	"fmt"
-	"log"
-
 	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/service/appstream"
+	"github.com/aws/aws-sdk-go-v2/service/appstream/types"
+	"log"
 )
 
 func main() {
-	stackName := flag.String("stack", "", "The name of the AppStream stack")
-	userName := flag.String("user", "", "The user name")
-	fleetName := flag.String("fleet", "", "Fleet name")
+	stackName := flag.String("stack", "3dPrinting", "The name of the AppStream stack")
+	userName := flag.String("user", "3dp", "user name prefix")
+	fleetName := flag.String("fleet", "3d_Printing", "Fleet name")
+	count := flag.Int64("count", 1, "Number of urls")
+	profileName := flag.String("profile", "personal", "AWS profile name")
 	flag.Parse()
 
-	if *stackName == "" || *userName == "" || *fleetName == "" {
+	if *stackName == "" || *userName == "" || *profileName == "" || *fleetName == "" {
 		log.Fatal("All flags are required")
 	}
 
-	cfg, err := config.LoadDefaultConfig(context.TODO())
+	ctx := context.Background()
+
+	profile := config.WithSharedConfigProfile(*profileName)
+	region := config.WithRegion("us-west-2")
+	cfg, err := config.LoadDefaultConfig(ctx, profile, region)
 	if err != nil {
 		log.Fatalf("Unable to load SDK config, %v", err)
 	}
 
 	client := appstream.NewFromConfig(cfg)
 
-	url, err := createStreamingURL(client, *stackName, *userName, *fleetName)
-	if err != nil {
-		log.Fatalf("Failed to create AppStream streaming URL: %v", err)
+	if err := startFleet(ctx, client, *fleetName); err != nil {
+		log.Fatalf("startfleet failed with %v", err)
 	}
 
-	fmt.Printf("Streaming URL: %s\n", url)
+	for i := 0; i < int(*count); i++ {
+		url, err := createStreamingURL(ctx, client, *stackName, *userName, *fleetName)
+		if err != nil {
+			log.Fatalf("Failed to create AppStream streaming URL: %v", err)
+		}
+
+		fmt.Printf("Streaming URL: %s\n", url)
+	}
 }
 
-func createStreamingURL(client *appstream.Client, stackName, userName, fleetName string) (string, error) {
+func startFleet(ctx context.Context, client *appstream.Client, fleetName string) error {
+	fleet := &appstream.DescribeFleetsInput{Names: []string{fleetName}}
+	fleets, err := client.DescribeFleets(ctx, fleet)
+	if err != nil {
+		return fmt.Errorf("DescribeFleets failed with err=%w", err)
+	}
+	for _, f := range fleets.Fleets {
+		if *f.Name == fleetName {
+			switch f.State {
+			case types.FleetStateRunning:
+				return nil
+			case types.FleetStateStopped:
+				fmt.Printf("Starting Fleet %v\n", *f.Name)
+				_, err := client.StartFleet(ctx, &appstream.StartFleetInput{Name: &fleetName})
+				if err != nil {
+					return fmt.Errorf("StartFleet failed with err=%w", err)
+				}
+				fmt.Printf("Started fleet")
+				return nil
+			default:
+				log.Fatalf("The Fleet state is %v, let that finish and try again...", f.State)
+			}
+		}
+	}
+	return nil
+}
+
+func createStreamingURL(ctx context.Context, client *appstream.Client, stackName, userName, fleetName string) (string, error) {
 	input := &appstream.CreateStreamingURLInput{
 		StackName: &stackName,
 		UserId:    &userName,
 		FleetName: &fleetName,
 	}
 
-	result, err := client.CreateStreamingURL(context.TODO(), input)
+	result, err := client.CreateStreamingURL(ctx, input)
 	if err != nil {
 		return "", err
 	}
